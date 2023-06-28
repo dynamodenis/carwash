41bf9c45e9999fbd100040fca25e37c35d59bd91
Dynamo Denis Mbugua <dmbugua66@gmail.com>
2023-06-28 10:00:03 +0300

    tests

--- src/main/java/com/carwash/carwash/web/CarwashController.java
+++ src/main/java/com/carwash/carwash/web/CarwashController.java
@@ -2,6 +2,8 @@ package com.carwash.carwash.web;
 
 import com.carwash.carwash.model.Car;
 import com.carwash.carwash.model.Driver;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -27,6 +29,22 @@ public class CarwashController {
         cars.add(car2);
 
         return cars;
+    }
+
+    @RequestMapping("/car/{id}")
+    public ResponseEntity<Car> getCar(@PathVariable("id") int id){
+        List<Car> cars = new ArrayList<Car>();
+
+        Driver driver1 = new Driver(1,"Denis", "Mbugua", "099999", "Kenya");
+        Driver driver2 = new Driver(2,"DOug", "Nje", "099999", "Uganda");
+
+        Car car1 = new Car(1, "Audi", "Black", driver1, "2012");
+        Car car2 = new Car(2, "Benz", "White", driver2, "2019");
+
+        cars.add(car1);
+        cars.add(car2);
+
+        return ResponseEntity.ok(cars.get(id));
     }
 
     @RequestMapping("/ford")
--- src/test/java/com/carwash/carwash/CarwashApplicationTests.java
+++ src/test/java/com/carwash/carwash/CarwashApplicationTests.java
@@ -1,13 +1,30 @@
 package com.carwash.carwash;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootTest
 class CarwashApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	RestTemplate restTemplate;
+
+	@BeforeAll
+	public void init(){
+		restTemplate = new RestTemplate();
+	}
+
+	@Test
+	public void getCarsTest(){
+		List cars = restTemplate.getForObject("http://localhost:8080/carwash/cars", List.class);
+		System.out.println("Cars length :" + cars.size());
 	}
 
 }
